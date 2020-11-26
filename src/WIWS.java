6bffe3033e908902ba02c88c3f1ee50446b0a7ca
Nonus10 <luka.krstik@protonmail.com>
2020-11-26 20:51:39 +0100

    se raboti ko so treba samo ostanuva kako file da go export

--- src/WIWS.java
+++ src/WIWS.java
@@ -22,16 +22,19 @@ public class WIWS {
             System.out.println("Done " + counter1);
         }
         //System.out.println(array1[1]);
-        /*for (int counter2 = 0; array1[counter2] != null; counter2++) {
-            System.out.println(array1[counter2]);
+        /*for (int counter3 = 0; array1[counter3] != null; counter3++) {
+            System.out.println(array1[counter3]);
         }*/
         System.out.println("================================================================");
         // Najkompliciranoto
         System.out.println("Image Compression...");
-        float quality = 0.2f;
+        float quality = 0.3f;
+        float qualityThumb = 0.1f;
+        String[] arrayComp = new String[50];
+        String[] arrayThumb = new String[50];
         for (int counter2 = 0; array1[counter2] != null; counter2++) {
             File slika = new File(array1[counter2]);
-            File compressedSlika = new File(array1[counter2] + "Thumb.jpg");
+            File compressedSlika = new File("Comp" + array1[counter2]);
 
             InputStream inputStream = new FileInputStream(slika);
             OutputStream outputStream = new FileOutputStream(compressedSlika);
@@ -39,7 +42,7 @@ public class WIWS {
             BufferedImage bufferSlika = ImageIO.read(inputStream);
             Iterator<ImageWriter> writers = ImageIO.getImageWritersByFormatName("jpg");
 
-            if(!writers.hasNext())
+            if (!writers.hasNext())
                 throw new IllegalStateException("Kraden Kod se razbira");
 
             ImageWriter writer = (ImageWriter) writers.next();
@@ -57,8 +60,57 @@ public class WIWS {
             imageOutputStream.close();
             writer.dispose();
 
-            System.out.println("Done "+ (counter2+1));
-        }
+            arrayComp[counter2] = "Comp" + array1[counter2];
+            System.out.println("File Name: " + arrayComp[counter2]);
 
+            System.out.println("Compressed: Done " + (counter2 + 1));
+            //========================================
+            File slikaThumb = new File(array1[counter2]);
+            File compressedSlikaThumb = new File("Thumb" + array1[counter2]);
+
+            InputStream inputStreamThumb = new FileInputStream(slikaThumb);
+            OutputStream outputStreamThumb = new FileOutputStream(compressedSlikaThumb);
+
+            BufferedImage bufferSlikaThumb = ImageIO.read(inputStreamThumb);
+            Iterator<ImageWriter> writersThumb = ImageIO.getImageWritersByFormatName("jpg");
+
+            if (!writersThumb.hasNext())
+                throw new IllegalStateException("Kraden Kod se razbira");
+
+            ImageWriter writerThumb = (ImageWriter) writersThumb.next();
+            ImageOutputStream imageOutputStreamThumb = ImageIO.createImageOutputStream(outputStreamThumb);
+            writerThumb.setOutput(imageOutputStreamThumb);
+
+            ImageWriteParam parametersThumb = writerThumb.getDefaultWriteParam();
+
+            parametersThumb.setCompressionMode(ImageWriteParam.MODE_EXPLICIT);
+            parametersThumb.setCompressionQuality(qualityThumb);
+            writerThumb.write(null, new IIOImage(bufferSlikaThumb, null, null), parametersThumb);
+
+            inputStreamThumb.close();
+            outputStreamThumb.close();
+            imageOutputStreamThumb.close();
+            writerThumb.dispose();
+
+            arrayThumb[counter2] = "Thumb" + array1[counter2];
+            System.out.println("File Name: " + arrayThumb[counter2]);
+
+            System.out.println("Thumnails: Done " + (counter2 + 1));
+        }
+        System.out.println("================================================================");
+        for (int counter3 = 0; arrayComp[counter3] != null && arrayThumb[counter3] != null; counter3++) {
+            arrayComp[counter3] = "https://github.com/lukakrstik/website-photo-pool/blob/master/images/" + arrayComp[counter3];
+            arrayThumb[counter3] = "https://github.com/lukakrstik/website-photo-pool/blob/master/images/" + arrayThumb[counter3];
+        }
+        System.out.println("All URLs Generated B)");
+        System.out.println("================================================================");
+
+        for (int counter4 = 0; arrayComp[counter4] != null && arrayThumb[counter4] != null; counter4++) {
+            System.out.println("<div class=\"card m-2\" style=\"border: none;\">\n"+
+            "\t<a href=\"" + arrayComp[counter4] + "\" data-fancybox=\"gallery\">\n"+
+            "\t\t<img class=\"card-img\" src= \"" + arrayThumb[counter4] + "\">\n"+
+            "\t</a>\n"+
+            "</div>\n\n");
+        }
     }
 }
